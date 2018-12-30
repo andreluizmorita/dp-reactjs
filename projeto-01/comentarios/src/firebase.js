41dd6a9b6ff102ad05a9f8859cbb8a6bc80a611e
André Luiz Morita <andreluizmorita@gmail.com>
2018-12-30 20:59:35 -0200

    Test

--- projeto-01/comentarios/src/firebase.js
+++ projeto-01/comentarios/src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/database";
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyAiMs15slwSEQrO1V9euiM7x3F-fGG3PB8",
-  authDomain: "dp-comentarios.firebaseapp.com",
-  databaseURL: "https://dp-comentarios.firebaseio.com",
-  projectId: "dp-comentarios",
-  storageBucket: "dp-comentarios.appspot.com",
-  messagingSenderId: "988567685204"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 firebase.initializeApp(config);
 
