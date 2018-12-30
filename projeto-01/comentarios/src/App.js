7e64b5ec9d66252a8b9545c92c52dc03b3a64883
André Luiz Morita <andreluizmorita@gmail.com>
2018-12-30 18:20:50 -0200

    Estrutura

---
+++ projeto-01/comentarios/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Comments from "./Comments";
+import NewComment from "./NewComment";
+
+class App extends Component {
+  state = {
+    comments: ["Comment 1", "Comment 2", "Comment 3"]
+  };
+
+  sendComment = comment => {
+    this.setState({
+      comments: [...this.state.comments, comment]
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <NewComment sendComment={this.sendComment} />
+        <Comments comments={this.state.comments} />
+      </div>
+    );
+  }
+}
+
+export default App;
