091cd9ae8d49840d544079bfaf7f65e16f65844f
André Luiz Morita <andreluizmorita@gmail.com>
2018-12-30 17:34:53 -0200

    Init comentários

--- projeto-01/counter/src/App.js
+++ projeto-01/counter/src/App.js
@@ -1,24 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    contador: 1
+  };
+
+  increment = () => {
+    this.setState({
+      contador: this.state.contador + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      contador: this.state.contador - 1
+    });
+  };
+
+  clear = () => {
+    this.setState({
+      contador: 0
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Contador: {this.state.contador}
+          <button onClick={this.increment}>+</button>
+          <button onClick={this.decrement}>-</button>
+          <button onClick={this.clear}>Clear</button>
         </header>
       </div>
     );
